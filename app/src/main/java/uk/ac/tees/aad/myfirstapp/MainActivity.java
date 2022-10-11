be0e0005e06f1d25983695451d2337d6f7635663
deda <adedamolafunmilayo@outlook.com>
2022-10-11 10:33:07 +0100

    1st commit

---
+++ app/src/main/java/uk/ac/tees/aad/myfirstapp/MainActivity.java
@@ -0,0 +1,25 @@
+package uk.ac.tees.aad.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onBtnClick (View view){
+        TextView txtHello = findViewById(R.id.txtMessage);
+        txtHello.setText("Hiyah");
+
+
+    }
+}
